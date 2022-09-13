4cf8e50e0f944479377e063a24a52d40d01a0c3e
Ed Wang <edwang@live.com>
2022-09-13 22:47:52 +0800

    update CreateOrderParams style

--- src/Digiflow.Payment/Models/Requests/Params/CreateOrderParams.cs
+++ src/Digiflow.Payment/Models/Requests/Params/CreateOrderParams.cs
@@ -1,60 +1,19 @@
-using System.ComponentModel.DataAnnotations;
-using Digiflow.Payment.Enums;
-using Refit;
-
 namespace Digiflow.Payment.Models.Requests.Params;
 
 public class CreateOrderParams
 {
-	[AliasAs("version")]
-	[MaxLength(3)]
-	public string? Version { get; set; } = "1.0";
-
-	[AliasAs("merchant_id")]
-	[MaxLength(15)]
-	public string? MerchantId { get; set; }
-
-	[AliasAs("terminal_id")]
-	[MaxLength(8)]
-	public string? TerminalId { get; set; }
-
-	[AliasAs("order_id")]
-	[MaxLength(32)]
-	public string? OrderId { get; set; }
-
-	[AliasAs("currency")]
-	public Currency Currency { get; set; } = Currency.TWD;
-
-	[AliasAs("order_amount")]
-	public int Amount { get; set; }
-
-	[AliasAs("order_desc")]
-	[MaxLength(64)]
-	public string? Description { get; set; }
-
-	[AliasAs("expiry_time")]
-	public string ExpiryTime { get; set; } = $"{DateTime.UtcNow.AddHours(8).AddDays(1):yyyyMMddHHmmss}";
-
-	[AliasAs("payment_type")]
-	public string? PaymentType { get; set; } = null;
-
-	[AliasAs("member_id")]
-	[MaxLength(48)]
-	public string? MemberId { get; set; } = null;
-
-	[AliasAs("buyer_mail")]
-	[EmailAddress, MaxLength(48)]
-	public string? BuyerEmail { get; set; } = null;
-
-	[AliasAs("ext_data")]
-	[MaxLength(256)]
-	public string? ExtraData { get; set; } = null;
-
-	[AliasAs("timestamp")]
-	public static string Timestamp =>
-		((long)(DateTime.UtcNow - new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc)).TotalMilliseconds).ToString();
-
-	[AliasAs("sign")]
-	[MaxLength(64)]
-	public string? Sign { get; set; }
+	public string? version { get; set; }
+	public string? merchant_id { get; set; }
+	public string? terminal_id { get; set; }
+	public string? order_id { get; set; }
+	public string? currency { get; set; }
+	public int? order_amount { get; set; }
+	public string? order_desc { get; set; }
+	public string? expiry_time { get; set; }
+	public string? payment_type { get; set; }
+	public string? member_id { get; set; }
+	public string? buyer_mail { get; set; }
+	public string? ext_data { get; set; }
+	public string? timestamp { get; set; }
+	public string? sign { get; set; }
 }
