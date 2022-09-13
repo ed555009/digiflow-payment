c03864456fbbe73cf5ca57c3a468b622ef7b35b7
Ed Wang <edwang@live.com>
2022-09-13 22:49:50 +0800

    fix api path

--- src/Digiflow.Payment/Interfaces/IPaymentApi.cs
+++ src/Digiflow.Payment/Interfaces/IPaymentApi.cs
@@ -7,7 +7,7 @@ namespace Digiflow.Payment.Interfaces;
 [Headers("User-Agent: Digiflow.PaymentApi", "Accept: application/json")]
 internal interface IPaymentApi
 {
-	[Post("universal/order")]
+	[Post("/order")]
 	[Headers("Content-Type: application/x-www-form-urlencoded;charset=utf-8")]
 	Task<ApiResponse<CreateOrderModel>> CreateOrderAsync([Query] CreateOrderParams data);
 }
