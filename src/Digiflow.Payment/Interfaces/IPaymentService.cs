a1ab26e9d62ca7a65b5ba5bbf510ecc9886af4a8
Ed Wang <edwang@live.com>
2022-09-14 21:27:24 +0800

    update PaymentService

--- src/Digiflow.Payment/Interfaces/IPaymentService.cs
+++ src/Digiflow.Payment/Interfaces/IPaymentService.cs
@@ -1,5 +1,9 @@
+using Digiflow.Payment.Models.Responses;
+using Refit;
+
 namespace Digiflow.Payment.Interfaces;
 
 public interface IPaymentService
 {
+	Task<ApiResponse<CreateOrderModel>> CreateOrderAsync(Models.Requests.CreateOrderModel data);
 }
--- src/Digiflow.Payment/Services/PaymentService.cs
+++ src/Digiflow.Payment/Services/PaymentService.cs
@@ -1,5 +1,9 @@
+using System.Security.Cryptography;
+using System.Text;
 using Digiflow.Payment.Configs;
+using Digiflow.Payment.Extensions;
 using Digiflow.Payment.Interfaces;
+using Digiflow.Payment.Models.Requests.Params;
 using Digiflow.Payment.Models.Responses;
 using Microsoft.Extensions.Logging;
 using Refit;
@@ -22,15 +26,45 @@ public class PaymentService : IPaymentService
 		_paymentApi = RestService.For<IPaymentApi>($"{paymentApiConfig.BaseUrl}/universal");
 	}
 
-	// public Task<ApiResponse<CreateOrderModel>> CreateOrderAsync(Models.Requests.CreateOrderModel data)
-	// {
-	// 	return _paymentApi.CreateOrderAsync(data);
-	// }
+	public async Task<ApiResponse<CreateOrderModel>> CreateOrderAsync(Models.Requests.CreateOrderModel data)
+	{
+		var param = new CreateOrderParams
+		{
+			// version = _paymentApiConfig.Version,
+			// merchant_id = _paymentApiConfig.MerchantId,
+			// terminal_id = _paymentApiConfig.TerminalId,
+			order_id = data.OrderId,
+			currency = data.Currency.ToDescription(),
+			order_amount = ((int)((data.Amount ?? 0) * 100m)).ToString(),
+			order_desc = data.Description,
+			expiry_time = data.ExpiryTime,
+			payment_type = ((int)data.PaymentType).ToString(),
+			member_id = data.MemberId,
+			buyer_mail = data.BuyerEmail,
+			ext_data = data.ExtraData,
+			timestamp = data.Timestamp
+		};
+
+		var signString = GetSignString(param);
+		using var sha256 = SHA256.Create();
+		param.sign = Convert.ToBase64String(sha256.ComputeHash(new UTF8Encoding().GetBytes(signString)));
+
+		return await _paymentApi.CreateOrderAsync(param);
+	}
 
 	void ValidateConfig()
 	{
 		ArgumentNullException.ThrowIfNull(_paymentApiConfig.MerchantId, nameof(_paymentApiConfig.MerchantId));
 		ArgumentNullException.ThrowIfNull(_paymentApiConfig.TerminalId, nameof(_paymentApiConfig.TerminalId));
 		ArgumentNullException.ThrowIfNull(_paymentApiConfig.SignKey, nameof(_paymentApiConfig.SignKey));
+	}
+
+	string GetSignString(IBaseQueryParam param)
+	{
+		param.version = _paymentApiConfig.Version;
+		param.merchant_id = _paymentApiConfig.MerchantId;
+		param.terminal_id = _paymentApiConfig.TerminalId;
+
+		return $"{param.ToOrderedValuesString()}&key={_paymentApiConfig.SignKey}";
 	}
 }
