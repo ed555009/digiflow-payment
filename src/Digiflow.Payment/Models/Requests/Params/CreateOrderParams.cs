8c543660acc48287a518d8e3a0fa36e163ab2f91
Ed Wang <ed555009@gmail.com>
2022-09-15 18:08:16 +0800

    fix CreateOrderParams

--- src/Digiflow.Payment/Models/Requests/Params/CreateOrderParams.cs
+++ src/Digiflow.Payment/Models/Requests/Params/CreateOrderParams.cs
@@ -2,7 +2,7 @@ namespace Digiflow.Payment.Models.Requests.Params;
 
 public class CreateOrderParams : BaseQueryParam
 {
-	public string? order_id { get; set; }
+	public string? order_no { get; set; }
 	public string? currency { get; set; }
 	public string? order_amount { get; set; }
 	public string? order_desc { get; set; }
