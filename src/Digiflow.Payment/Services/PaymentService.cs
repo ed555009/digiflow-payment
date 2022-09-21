0cc787490fb2ed469434dea8c8e443a871f78df6
Ed Wang <ed555009@gmail.com>
2022-09-21 14:42:12 +0800

    reduce duplicate code

--- src/Digiflow.Payment/Services/PaymentService.cs
+++ src/Digiflow.Payment/Services/PaymentService.cs
@@ -21,22 +21,15 @@ public sealed class PaymentService : IPaymentService
 		_paymentApi = RestService.For<IPaymentApi>($"{paymentApiConfig.BaseUrl}/universal");
 	}
 
-	public async Task<ApiResponse<CancelOrderModel>> CancelOrderAsync(Models.Requests.QueryOrderModel data)
-	{
-		var param = new QueryOrderParams
+	public async Task<ApiResponse<CancelOrderModel>> CancelOrderAsync(Models.Requests.QueryOrderModel data) =>
+		await _paymentApi.CancelOrderAsync(SignParam<QueryOrderParams>(new QueryOrderParams
 		{
 			order_no = data.OrderNo,
 			timestamp = data.Timestamp
-		};
+		}));
 
-		param.sign = _signService.Sign(param);
-
-		return await _paymentApi.CancelOrderAsync(param);
-	}
-
-	public async Task<ApiResponse<CreateOrderModel>> CreateOrderAsync(Models.Requests.CreateOrderModel data)
-	{
-		var param = new CreateOrderParams
+	public async Task<ApiResponse<CreateOrderModel>> CreateOrderAsync(Models.Requests.CreateOrderModel data) =>
+		await _paymentApi.CreateOrderAsync(SignParam<CreateOrderParams>(new CreateOrderParams
 		{
 			order_no = data.OrderNo,
 			currency = data.Currency.ToDescription(),
@@ -48,37 +41,27 @@ public sealed class PaymentService : IPaymentService
 			buyer_mail = data.BuyerEmail,
 			ext_data = data.ExtraData,
 			timestamp = data.Timestamp
-		};
+		}));
 
-		param.sign = _signService.Sign(param);
-
-		return await _paymentApi.CreateOrderAsync(param);
-	}
-
-	public async Task<ApiResponse<QueryOrderModel>> QueryOrderAsync(Models.Requests.QueryOrderModel data)
-	{
-		var param = new QueryOrderParams
+	public async Task<ApiResponse<QueryOrderModel>> QueryOrderAsync(Models.Requests.QueryOrderModel data) =>
+		await _paymentApi.QueryOrderAsync(SignParam<QueryOrderParams>(new QueryOrderParams
 		{
 			order_no = data.OrderNo,
 			timestamp = data.Timestamp
-		};
+		}));
 
-		param.sign = _signService.Sign(param);
-
-		return await _paymentApi.QueryOrderAsync(param);
-	}
-
-	public async Task<ApiResponse<RefundOrderModel>> RefundOrderAsync(Models.Requests.RefundOrderModel data)
-	{
-		var param = new RefundOrderParams
+	public async Task<ApiResponse<RefundOrderModel>> RefundOrderAsync(Models.Requests.RefundOrderModel data) =>
+		await _paymentApi.RefundOrderAsync(SignParam<RefundOrderParams>(new RefundOrderParams
 		{
 			order_no = data.OrderNo,
 			refund_amount = ((int)((data.Amount ?? 0) * 100m)).ToString(),
 			timestamp = data.Timestamp
-		};
+		}));
 
+	T SignParam<T>(IBaseQueryParam param) where T : IBaseQueryParam
+	{
 		param.sign = _signService.Sign(param);
 
-		return await _paymentApi.RefundOrderAsync(param);
+		return (T)param;
 	}
 }
